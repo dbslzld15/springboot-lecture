7f55f5c18a3d93be30c491ef82cb8dc1d604a334
dbslzld15 <dbslzld15@naver.com>
2022-04-04 18:24:18 +0900

    fix: 주문생성 테스트 코드 수정

--- spring-boot-part1/kdt-spring-order/src/test/java/org/prgrms/kdt/order/OrderTest.java
+++ spring-boot-part1/kdt-spring-order/src/test/java/org/prgrms/kdt/order/OrderTest.java
@@ -1,5 +1,6 @@
 package org.prgrms.kdt.order;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.prgrms.kdt.voucher.FixedAmountVoucher;
 
@@ -7,18 +8,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
 
     @Test
-    public void 바우처설계() throws Exception {
+    public void 바우처를_통해_주문생성() throws Exception {
         UUID customerId = UUID.randomUUID();
+        OrderContext orderContext = new OrderContext();
+        OrderService orderService = orderContext.orderService();
         List<OrderItem> orderItems = new ArrayList<>() {{
             add(new OrderItem(UUID.randomUUID(), 100L, 1));
         }};
-        FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L);
-        Order order = new Order(UUID.randomUUID(), customerId, orderItems, fixedAmountVoucher);
+
+        Order order1 = orderService.createOrder(customerId, orderItems);
+        assertThat(order1.totalAmount()).isEqualTo(100L);
+
+        FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L); //고정 할인
+        Order order2 = new Order(UUID.randomUUID(), customerId, orderItems, fixedAmountVoucher);
+        assertThat(order2.totalAmount()).isEqualTo(90L);
     }
 
 }
