308316fc094cb03e688c65b488636ff3fe50bad3
dbslzld15 <dbslzld15@naver.com>
2022-04-04 18:23:02 +0900

    feat: 바우처 활용, 주문 생성 코드 구현

---
+++ spring-boot-part1/kdt-spring-order/src/main/java/org/prgrms/kdt/order/OrderService.java
@@ -0,0 +1,31 @@
+package org.prgrms.kdt.order;
+
+import org.prgrms.kdt.voucher.Voucher;
+import org.prgrms.kdt.voucher.VoucherService;
+
+import java.util.List;
+import java.util.UUID;
+
+public class OrderService {
+    private final VoucherService voucherService;
+    private final OrderRepository orderRepository;
+
+    public OrderService(VoucherService voucherService, OrderRepository orderRepository) {
+        this.voucherService = voucherService;
+        this.orderRepository = orderRepository;
+    }
+
+    public Order createOrder(UUID customerId, List<OrderItem> orderItems) { // 바우처가 없을때도 주문 생성이 가능하다.
+        Order order = new Order(UUID.randomUUID(), customerId, orderItems);
+        orderRepository.insert(order);
+        return order;
+    }
+
+    public Order createOrder(UUID customerId, List<OrderItem> orderItems, UUID voucherId) {
+        Voucher voucher = voucherService.getVoucher(voucherId);
+        Order order = new Order(UUID.randomUUID(), customerId, orderItems, voucher);
+        orderRepository.insert(order);
+        voucherService.useVoucher(voucher); //바우처를 사용했다고 기록해줘야함
+        return order;
+    }
+}
---
+++ spring-boot-part1/kdt-spring-order/src/main/java/org/prgrms/kdt/voucher/VoucherRepository.java
@@ -0,0 +1,9 @@
+package org.prgrms.kdt.voucher;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface VoucherRepository { //DB를 통해 어떻게 보관하느냐에 따라 바뀔수 있음 (mysql, mongoDB..)
+    // 그래서 레포에 대한 구현체를 별도로 가져감
+    Optional<Voucher> findById(UUID voucherId);
+}
