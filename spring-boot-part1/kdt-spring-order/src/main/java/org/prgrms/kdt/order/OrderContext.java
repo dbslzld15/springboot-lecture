d8b4bb9ce87268303d202b013e231009ea9ad64b
dbslzld15 <dbslzld15@naver.com>
2022-04-04 18:23:55 +0900

    feat: 주문 도메인에 대한 생성을 책임지는 Context 객체 구현(IOC)

---
+++ spring-boot-part1/kdt-spring-order/src/main/java/org/prgrms/kdt/order/OrderContext.java
@@ -0,0 +1,37 @@
+package org.prgrms.kdt.order;
+
+import org.prgrms.kdt.voucher.Voucher;
+import org.prgrms.kdt.voucher.VoucherRepository;
+import org.prgrms.kdt.voucher.VoucherService;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class OrderContext { // 주문에 대한 전반적인 도메인 객체에 대한 생성을 책임짐, IOC라고 부름
+
+    public VoucherRepository voucherRepository() {
+        return new VoucherRepository() {
+            @Override
+            public Optional<Voucher> findById(UUID voucherId) {
+                return Optional.empty();
+            }
+        };
+    }
+
+    public OrderRepository orderRepository() {
+        return new OrderRepository() {
+            @Override
+            public void insert(Order order) {
+
+            }
+        };
+    }
+
+    public VoucherService voucherService() {
+        return new VoucherService(voucherRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderService(voucherService(), orderRepository());
+    }
+}
