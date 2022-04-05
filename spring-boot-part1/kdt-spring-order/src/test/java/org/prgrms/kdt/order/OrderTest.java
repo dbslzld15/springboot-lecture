7baaf2b1c1aa242c75832ac3d8fab3ab416b9454
dbslzld15 <dbslzld15@naver.com>
2022-04-05 18:11:08 +0900

    feat: 메모리 저장소 구현

--- spring-boot-part1/kdt-spring-order/src/test/java/org/prgrms/kdt/order/OrderTest.java
+++ spring-boot-part1/kdt-spring-order/src/test/java/org/prgrms/kdt/order/OrderTest.java
@@ -3,6 +3,9 @@ package org.prgrms.kdt.order;
 import org.junit.jupiter.api.Test;
 import org.prgrms.kdt.AppConfiguration;
 import org.prgrms.kdt.voucher.FixedAmountVoucher;
+import org.prgrms.kdt.voucher.Voucher;
+import org.prgrms.kdt.voucher.VoucherRepository;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.ArrayList;
@@ -15,9 +18,13 @@ class OrderTest {
 
     @Test
     public void 바우처를_통해_주문생성() throws Exception {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfiguration.class);
-
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfiguration.class);
         UUID customerId = UUID.randomUUID();
+
+        Voucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L); //고정 할인
+        VoucherRepository voucherRepository = ac.getBean(VoucherRepository.class);
+        Voucher voucher = voucherRepository.insert(fixedAmountVoucher);
+
         OrderService orderService = ac.getBean(OrderService.class);
         List<OrderItem> orderItems = new ArrayList<>() {{
             add(new OrderItem(UUID.randomUUID(), 100L, 1));
@@ -26,8 +33,7 @@ class OrderTest {
         Order order1 = orderService.createOrder(customerId, orderItems);
         assertThat(order1.totalAmount()).isEqualTo(100L);
 
-        FixedAmountVoucher fixedAmountVoucher = new FixedAmountVoucher(UUID.randomUUID(), 10L); //고정 할인
-        Order order2 = new Order(UUID.randomUUID(), customerId, orderItems, fixedAmountVoucher);
+        Order order2 = orderService.createOrder(customerId, orderItems, voucher.getVoucherId());
         assertThat(order2.totalAmount()).isEqualTo(90L);
     }
 
