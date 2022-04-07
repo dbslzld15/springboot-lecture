b597a0162da954d7f463f69d927ce38fceeca1ab
dbslzld15 <dbslzld15@naver.com>
2022-04-07 15:51:37 +0900

    feat: application.properties 활용해 환경변수 가져오기 구현

--- spring-boot-part1/kdt-spring-order/src/main/java/org/prgrms/kdt/AppConfiguration.java
+++ spring-boot-part1/kdt-spring-order/src/main/java/org/prgrms/kdt/AppConfiguration.java
@@ -2,9 +2,11 @@ package org.prgrms.kdt;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan
+@PropertySource("application.properties")
 public class AppConfiguration { // 주문에 대한 전반적인 도메인 객체에 대한 생성을 책임짐, IOC라고 부름
 
 }
