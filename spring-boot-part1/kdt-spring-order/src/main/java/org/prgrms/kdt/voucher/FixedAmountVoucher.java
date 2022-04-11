c70db2976612d3b5a0565da7cc04758ff9c32789
dbslzld15 <dbslzld15@naver.com>
2022-04-11 17:03:36 +0900

    feat: 바우처 할인관련 검증 로직 추가

--- spring-boot-part1/kdt-spring-order/src/main/java/org/prgrms/kdt/voucher/FixedAmountVoucher.java
+++ spring-boot-part1/kdt-spring-order/src/main/java/org/prgrms/kdt/voucher/FixedAmountVoucher.java
@@ -3,10 +3,20 @@ package org.prgrms.kdt.voucher;
 import java.util.UUID;
 
 public class FixedAmountVoucher implements Voucher{
+    private static final long MAX_VOUCHER_AMOUNT = 10000;
     private final UUID voucherId;
     private final long amount;
 
     public FixedAmountVoucher(UUID voucherId, long amount) {
+        if(amount < 0){
+            throw new IllegalArgumentException("Should me positive value");
+        }
+        if(amount == 0) {
+            throw new IllegalArgumentException("Amount should not be zero");
+        }
+        if(amount > MAX_VOUCHER_AMOUNT) {
+            throw new IllegalArgumentException("Amount should be less than %d".formatted(MAX_VOUCHER_AMOUNT));
+        }
         this.voucherId = voucherId;
         this.amount = amount;
     }
@@ -17,7 +27,8 @@ public class FixedAmountVoucher implements Voucher{
     }
 
     public long discount(long beforeDiscount) {
-        return beforeDiscount - amount;
+        long discountedPrice = beforeDiscount - amount;
+        return discountedPrice < 0 ? 0 : discountedPrice;
     }
 
 }
