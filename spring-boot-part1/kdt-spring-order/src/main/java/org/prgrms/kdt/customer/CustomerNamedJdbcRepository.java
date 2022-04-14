66e631730c4006b64b49b0e83a10b76644014608
dbslzld15 <dbslzld15@naver.com>
2022-04-14 16:09:23 +0900

    feat: named jdbc template 활용, crud 기능 구현

---
+++ spring-boot-part1/kdt-spring-order/src/main/java/org/prgrms/kdt/customer/CustomerNamedJdbcRepository.java
@@ -0,0 +1,134 @@
+package org.prgrms.kdt.customer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.nio.ByteBuffer;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Repository
+public class CustomerNamedJdbcRepository implements CustomerRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerNamedJdbcRepository.class);
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public CustomerNamedJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Customer insert(Customer customer) {
+        Map<String, Object> paramMap = toParamMap(customer);
+        int update = jdbcTemplate.update("insert into customers(customer_id, name, email, created_at) values UNHEX(REPLACE(:customerId, '-', ''), :name, :email, :createdAt)",
+                paramMap);
+        if(update != 1) {
+            throw new RuntimeException("Nothing was inserted");
+        }
+        return customer;
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        Map<String, Object> paramMap = toParamMap(customer);
+        int update = jdbcTemplate.update("update customers SET name = :name, email = :email, last_login_at = :lastLoginAt where customer_id = UNHEX(REPLACE(:customerId, '-', ''))",
+                paramMap);
+        if(update != 1) {
+            throw new RuntimeException("Nothing was inserted");
+        }
+        return customer;
+    }
+
+    @Override
+    public int count() {
+        return jdbcTemplate.queryForObject("select count(*) from customers", Collections.EMPTY_MAP, Integer.class);
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return jdbcTemplate.query("select * from customers", (rs, rowNum) -> rowMapperCustomer(rs));
+    }
+
+    @Override
+    public Optional<Customer> findById(UUID customerId) {
+        try{
+            Customer customer = jdbcTemplate.queryForObject(
+                    "select * from customers where customer_id = UNHEX(REPLACE(:customerId, '-', ''))",
+                    Collections.singletonMap("customerId", customerId.toString().getBytes()),
+                    (rs, rowNum) -> rowMapperCustomer(rs));
+            return Optional.of(customer);
+        } catch (EmptyResultDataAccessException e) {
+            logger.error("Got empty result", e);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Customer> findByName(String name) {
+        try{
+            Customer customer = jdbcTemplate.queryForObject(
+                    "select * from customers where name = :name",
+                    Collections.singletonMap("name", name),
+                    (rs, rowNum) -> rowMapperCustomer(rs));
+            return Optional.of(customer);
+        } catch (EmptyResultDataAccessException e) {
+            logger.error("Got empty result", e);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Customer> findByEmail(String email) {
+        try{
+            Customer customer = jdbcTemplate.queryForObject(
+                    "select * from customers where email = :email",
+                    Collections.singletonMap("email", email),
+                    (rs, rowNum) -> rowMapperCustomer(rs));
+            return Optional.of(customer);
+        } catch (EmptyResultDataAccessException e) {
+            logger.error("Got empty result", e);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        jdbcTemplate.update("delete from customers", Collections.EMPTY_MAP);
+    }
+
+    static UUID toUUID(byte[] bytes) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    }
+
+    private Customer rowMapperCustomer(ResultSet resultSet) throws SQLException {
+        UUID customerId = toUUID(resultSet.getBytes("customer_id"));
+        String customerName = resultSet.getString("name");
+        String email = resultSet.getString("email");
+        LocalDateTime lastLoginAt = resultSet.getTimestamp("last_login_at") != null ?
+                resultSet.getTimestamp("last_login_at").toLocalDateTime() : null;
+        LocalDateTime createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
+        return new Customer(customerId, customerName, email, lastLoginAt, createdAt);
+    }
+
+    private Map<String, Object> toParamMap(Customer customer) {
+        Map<String, Object> paramMap = new HashMap<>() {{
+            put("customerId", customer.getCustomerId().toString().getBytes());
+            put("name", customer.getName());
+            put("email", customer.getEmail());
+            put("createdAt", Timestamp.valueOf(customer.getCreatedAt()));
+            put("lastLoginAt", customer.getLastLoginAt() != null ? Timestamp.valueOf(customer.getLastLoginAt()) : null);
+        }};
+        return paramMap;
+    }
+
+}
---
+++ spring-boot-part1/kdt-spring-order/src/test/java/org/prgrms/kdt/customer/CustomerNamedJdbcRepositoryTest.java
@@ -0,0 +1,164 @@
+package org.prgrms.kdt.customer;
+
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.config.Charset;
+import com.wix.mysql.config.MysqldConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+import static com.wix.mysql.ScriptResolver.classPathScript;
+import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
+import static com.wix.mysql.distribution.Version.v5_7_latest;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+@SpringJUnitConfig
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CustomerNamedJdbcRepositoryTest {
+
+    @Configuration
+    @ComponentScan(
+            basePackages = {"org.prgrms.kdt.customer"}
+    )
+    static class config {
+
+        @Bean
+        public DataSource dataSource() {
+            HikariDataSource dataSource = DataSourceBuilder.create()
+                    .url("jdbc:mysql://localhost:2215/test-order_mgmt")
+                    .username("park")
+                    .password("1234")
+                    .type(HikariDataSource.class)
+                    .build();
+//            dataSource.setMaximumPoolSize(100);
+//            dataSource.setMinimumIdle(100);
+            return dataSource;
+        }
+        @Bean
+        public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+            return new JdbcTemplate(dataSource);
+        }
+
+        @Bean
+        public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) {
+            return new NamedParameterJdbcTemplate(jdbcTemplate);
+        }
+    }
+
+    @Autowired
+    CustomerJdbcTemplateRepository customerJdbcRepository;
+
+    @Autowired
+    DataSource dataSource;
+
+    Customer newCustomer;
+
+    EmbeddedMysql embeddedMysql;
+
+    @BeforeAll
+    void setUp() {
+        MysqldConfig mysqldConfig = aMysqldConfig(v5_7_latest)
+                .withCharset(Charset.UTF8)
+                .withPort(2215)
+                .withUser("park", "1234")
+                .withTimeZone("Asia/Seoul")
+                .build();
+        embeddedMysql = anEmbeddedMysql(mysqldConfig)
+                .addSchema("test-order_mgmt", classPathScript("schema.sql"))
+                .start();
+
+        newCustomer = new Customer(UUID.randomUUID(), "test-user", "test-user@gmail.com", LocalDateTime.now());
+    }
+
+    @AfterAll
+    void cleanUp() {
+        embeddedMysql.stop();
+    }
+
+    @Test
+    @Order(1)
+    @Disabled
+    public void HikariDataSource() throws Exception {
+        //given
+        //when
+        //then
+        assertThat(dataSource.getClass().getName(), is("com.zaxxer.hikari.HikariDataSource"));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("고객을 추가할 수 있다.")
+    public void testInsert() throws Exception {
+        customerJdbcRepository.insert(newCustomer);
+        Optional<Customer> retrievedCustomer = customerJdbcRepository.findById(newCustomer.getCustomerId());
+        assertThat(retrievedCustomer.isEmpty(), is(false));
+        assertThat(newCustomer.getCustomerId(), is(retrievedCustomer.get().getCustomerId()));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("전체 고객을 조회할 수 있다.")
+    public void testFindAll() throws Exception {
+        //given
+        List<Customer> customers = customerJdbcRepository.findAll();
+        customers.forEach(System.out::println);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("이름으로 고객을 조회할 수 있다.")
+    public void testFindByName() throws Exception {
+        //given
+        Optional<Customer> customer = customerJdbcRepository.findByName(newCustomer.getName());
+        Optional<Customer> unknown = customerJdbcRepository.findByName("unknown");
+        //when
+        //then
+        assertThat(customer.isEmpty(), is(false));
+        assertThat(unknown.isEmpty(), is(true));
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("이메일로 고객을 조회할 수 있다.")
+    public void testFindByEmail() throws Exception {
+        //given
+        Optional<Customer> email = customerJdbcRepository.findByEmail(newCustomer.getEmail());
+        Optional<Customer> unknown = customerJdbcRepository.findByEmail("unknown");
+        //when
+        //then
+        assertThat(email.isEmpty(), is(false));
+        assertThat(unknown.isEmpty(), is(true));
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("고객을 수정할 수 있다.")
+    public void testUpdateCustomer() throws Exception {
+        newCustomer.changeName("updated-user");
+        customerJdbcRepository.update(newCustomer);
+
+        List<Customer> customers = customerJdbcRepository.findAll();
+        assertThat(customers, hasSize(1));
+
+        Optional<Customer> retrievedCustomer = customerJdbcRepository.findById(newCustomer.getCustomerId());
+        assertThat(retrievedCustomer.isEmpty(), is(false));
+    }
+
+}
